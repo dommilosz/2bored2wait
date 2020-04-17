862671255c14c138bbc84711fc702975745d06e7
dommilosz <dommilosz@gmail.com>
2020-04-17 22:28:13 +0200

    upload

--- download-and-run.js
+++ download-and-run.js
@@ -10,6 +10,7 @@ var f1 = "main.js";
 var f2 = "webserver.js";
 var f3 = "index.html";
 var f4 = "index.css";
+var f5 = "package.json";
 var url = "https://raw.githubusercontent.com/dommilosz/2bored2wait/master/";
 
 options.filename = f1;
@@ -20,6 +21,8 @@ options.filename = f3;
 download(url + f3, options);
 options.filename = f4;
 download(url + f4, options);
+options.filename = f5;
+download(url + f5, options);
 
 
 var script;
