85a970aca1af2c4d76180d0a437ad810b9dcc959
dommilosz <dommilosz@gmail.com>
2020-04-22 19:33:32 +0200

    upload

--- download-and-run.js
+++ download-and-run.js
@@ -1,48 +1,52 @@
-var fs = require("fs");
-var file = "./main.js";
+var fs = require('fs');
+var file = './main.js';
 var download = require("download-file");
 
 var options = {
 	directory: process.cwd(),
 	filename: "main.js",
 };
-
-var files = [
-	"main.js",
-	"webserver.js",
-	"index.html",
-	"index.css",
-	"package.json",
-	"login.html",
-	"download-and-run.js",
-];
-files.forEach(DownloadFile);
-
+var f1 = "main.js";
+var f2 = "webserver.js";
+var f3 = "index.html";
+var f4 = "index.css";
+var f5 = "package.json";
+var f6 = "login.html";
 var url = "https://raw.githubusercontent.com/dommilosz/2bored2wait/master/";
 
-function DownloadFile(item, index) {
-	options.filename = item;
-	download(url + item, options);
-}
+DownloadFile(f1);
+DownloadFile(f2);
+DownloadFile(f3);
+DownloadFile(f4);
+DownloadFile(f5);
+DownloadFile(f6);
 
 console.log("Downloaded");
 
+function DownloadFile(file){
+  options.filename = file;
+  download(url + file, options);
+  console.log("Downloading : "+file);
+}
+
 var script;
 function loadScript() {
-	if (script) {
-		if (typeof script.teardown === "function") {
-			script.teardown();
-		}
-		delete require.cache[file];
-	}
+  if (script) {
+    if (typeof script.teardown === 'function') {
+      script.teardown();
+    }
+    delete require.cache[file];
+  }
 
-	script = require(file);
+  script = require(file);
 }
-setTimeout(function () {
-	try {
-		console.log("Trying to run app");
-		loadScript();
-		console.log("Succes");
-		console.log("");
-	} catch {}
-}, 1000);
+setTimeout(function(){
+  try{
+    console.log("Trying to run app");
+    loadScript();
+    console.log("Succes");
+    console.log("");
+  }catch{}
+},1000)
+
+
